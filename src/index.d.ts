648d6fb108a5fc10e00427f1bc3ca429cbdfe24c
Eric-LLi <ericli1116@gmail.com>
2021-03-02 08:53:17 +1100

    <UPDATE>

--- src/index.d.ts
+++ src/index.d.ts
@@ -41,6 +41,8 @@ export declare function on(event: READER_EVENTS, callback: Callbacks): void;
 
 export declare function off(event: READER_EVENTS): void;
 
+export declare function removeAll(event: READER_EVENTS): void;
+
 export declare function connect(name: string): Promise<boolean>;
 
 export declare function reconnect(): void;
--- src/index.js
+++ src/index.js
@@ -13,11 +13,19 @@ ZebraRfd8500.on = (event, handler) => {
 };
 
 ZebraRfd8500.off = (event) => {
-	if (events.hasOwnProperty(event)) {
+	if (Object.hasOwnProperty.call(events, event)) {
 		const eventListener = events[event].shift();
 
 		if(eventListener) eventListener.remove();
 	}
 };
+
+ZebraRfd8500.removeAll = (event) => {
+	if (Object.hasOwnProperty.call(events, event)) {
+		eventEmitter.removeAllListeners(event);
+
+		events[event] = [];
+	}
+}
 
 export default ZebraRfd8500;
